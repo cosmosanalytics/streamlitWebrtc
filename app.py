156fce633946df4772efe2fd1f0830b8e229b123
cosmosanalytics <100080151+cosmosanalytics@users.noreply.github.com>
2025-01-19 13:00:14 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,4 +4,9 @@ from streamlit_webrtc import webrtc_streamer
 st.title("My first Streamlit app")
 st.write("Hello, world")
 
-webrtc_streamer(key="example")
+webrtc_streamer(
+    key="example",
+    rtc_configuration={  # Add this line
+        "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
+    }
+)
